cf51effb06b127286b47f66e8008ac1ac032b8fd
erdemsevikk <erdemsevikk@gmail.com>
2021-11-22 20:01:26 +0300

    düzeltme işlemleri yapıldı

--- MCD_ArrayListOdev2/Program.cs
+++ MCD_ArrayListOdev2/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.Threading;
 
 namespace MCD_ArrayListOdev2
 {
@@ -10,7 +12,42 @@ namespace MCD_ArrayListOdev2
     {
         static void Main(string[] args)
         {
+            //ArrayList te tip yok.
+            ArrayList listem = new ArrayList();
+            listem.Add("Faik");
+            listem.Add("Turan");
+            listem.Add("123456789");
+            listem.Add('E');
+            listem.Add(20);
 
+            listem.Add("Esra");
+            listem.Add("Turan");
+            listem.Add("123456789");
+            listem.Add('K');
+            listem.Add(20);
+
+            listem.Add("Arif");
+            listem.Add("Turan");
+            listem.Add("123456789");
+            listem.Add('E');
+            listem.Add(20);
+
+            Console.WriteLine("ArrayList Oluşturuldu.");
+            Console.WriteLine("Yazdırılıyor...");
+            Thread.Sleep(1000);
+            foreach (var item in listem)
+            {
+                Console.WriteLine(item);
+            }
+           
+            Console.WriteLine("ArrayList kapasitesi = " + listem.Capacity);
+            listem.Remove("Faik");
+
+
+            Array.ForEach(listem.ToArray(), x => Console.WriteLine(x));
+
+
+            Console.ReadLine();
         }
     }
 }
